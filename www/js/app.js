41f20a5df6650fffc80520e002778a6301c7a0d3
remlamartisefi <remi.lamarti@laposte.net>
2017-03-14 12:12:17 +0100

    SeekFriend v1.0

---
+++ www/js/app.js
@@ -0,0 +1,95 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  })
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('map', {
+    url: '/',
+    templateUrl: 'templates/map.html',
+    controller: 'MapCtrl'
+  });
+
+  $urlRouterProvider.otherwise("/");
+
+})
+
+
+
+
+
+
+
+
+
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation,$ionicPopup) {
+  var options = {timeout: 10000, enableHighAccuracy: true};
+
+
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+    
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var coord = new google.maps.LatLng(47.389982, 0.688877);
+
+
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    //Wait until the map is loaded
+    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+
+      var marker = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: latLng
+      });  
+
+      var marker2 = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: coord
+      });      
+
+      var infoWindow = new google.maps.InfoWindow({
+          content: "Here I am!"
+      });
+
+      google.maps.event.addListener(marker2, 'click', function () {
+          infoWindow.open($scope.map, marker2);
+      }); 
+
+    });
+
+  }, function(error){
+    console.log("Could not get location");
+  });
+
+
+ 
+});
+
+
+
